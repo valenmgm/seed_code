bf689b784e175e89c794994ff3cf173d328df9d4
Fernanda Rodriguez Hernandez <fernandardzh@gmail.com>
2016-06-06 20:50:33 -0500

    algo

--- seed_code/catalogo/models.py
+++ seed_code/catalogo/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Catalogo(models.Model):
+    #Atributos
+    titulo = models.CharField(max_length = 120, blank = False)
+	    
+	class rotacion
+		titulo = models.CharField(max_length = 120, blank = False)
+		texto = models.CharField(max_length = 120, blank = False)
+
+	class riego
+
+
+	class composta         
+
+
+
+
+
+
+def __str__(self):
+        return unicode(self.titulo)
