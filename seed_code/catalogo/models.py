294733e4e0aa2a1af4b52f88193e50b264fc77c2
Fernanda Rodriguez Hernandez <fernandardzh@gmail.com>
2016-06-06 21:28:34 -0500

    m

--- seed_code/catalogo/models.py
+++ seed_code/catalogo/models.py
@@ -2,23 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Categoria(model.Model):
+	titulo = models.CharField(max_length = 120, blank = True, null=True)
+	def __str__(self):
+		return unicode(self.titulo)
+
 class Catalogo(models.Model):
-    #Atributos
-    titulo = models.CharField(max_length = 120, blank = False)
-	    
-	class rotacion
-		titulo = models.CharField(max_length = 120, blank = False)
-		texto = models.CharField(max_length = 120, blank = False)
-
-	class riego
-
-
-	class composta         
-
-
-
-
-
+	#Atributos
+	titulo = models.CharField(max_length = 120, blank = True, null = True)
+	categoria = models.ForeignKey(Categoria, related_name='categoria')
+	texto = models.TextField(blank =True, null=True)
 
 def __str__(self):
         return unicode(self.titulo)
